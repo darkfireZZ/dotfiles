30820c7496a3941331d1c4523dae9cc06297296d
darkfireZZ <64635413+darkfireZZ@users.noreply.github.com>
2022-10-18 16:51:12 +0200

    add first home manager configuration

---
+++ .config/nixpkgs/home.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "nicolabruhin";
+  home.homeDirectory = "/Users/nicolabruhin";
+
+  home.packages = [
+    pkgs.tree
+    pkgs.htop
+    pkgs.neovim
+    pkgs.hyperfine
+    pkgs.texlive.combined.scheme-medium
+  ];
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
