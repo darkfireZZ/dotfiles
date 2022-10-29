a3f0f77980202a10bc18663ea07911dd6d963c12
darkfireZZ <64635413+darkfireZZ@users.noreply.github.com>
2022-10-29 15:22:51 +0200

    restructure home.nix

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -40,6 +40,10 @@
       };
     };
 
+    home-manager = {
+      enable = true;
+    };
+
     kitty = {
       enable = true;
       settings = {
@@ -59,7 +63,4 @@
   # the Home Manager release notes for a list of state version
   # changes in each release.
   home.stateVersion = "22.05";
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
 }
