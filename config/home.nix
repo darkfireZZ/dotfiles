ccfb36f96e507ec3d845a403db5988a6822dfd14
darkfireZZ <64635413+darkfireZZ@users.noreply.github.com>
2022-11-01 19:35:37 +0100

    add qutebrowser configs

--- config/home.nix
+++ config/home.nix
@@ -34,6 +34,11 @@ in {
     source = "${dotfiles_dir}/config/nvim";
   };
 
+  # qutebrowser config
+  home.file.".qutebrowser" = {
+    source = "${dotfiles_dir}/config/qutebrowser";
+  };
+
   programs = {
     bash = {
       enable = true;
