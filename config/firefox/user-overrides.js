4be2b61feddda4cf68a6ce0c74fbe47a9e3428cf
darkfireZZ <64635413+darkfireZZ@users.noreply.github.com>
2023-10-28 08:49:05 +0200

    Don't restore firefox session after crash

--- config/firefox/user-overrides.js
+++ config/firefox/user-overrides.js
@@ -1,17 +1,22 @@
 
 //=======// Disable unnecessary firefox bloat that clutters the UI //=======//
 
-// Use this? Hide firefox view
+// Hide firefox view
 user_pref("browser.tabs.firefox-view", false);
+
 // Don't show suggested pages on about:home
 user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+
 // Disable firefox pocket
 user_pref("extensions.pocket.enabled", false);
+
+// Always hide the booksmarks toolbar
+user_pref("browser.toolbars.bookmarks.visibility", "never");
 
 //=======// Other settings //=======//
 
 // Don't ask to save logins and passwords for websites
 user_pref("signon.rememberSignons", false);
 
-// Always hide the booksmarks toolbar
-user_pref("browser.toolbars.bookmarks.visibility", "never");
+// disable resuming session from crash
+user_pref("browser.sessionstore.resume_from_crash", false);
