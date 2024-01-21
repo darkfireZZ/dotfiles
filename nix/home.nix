9d7fd037de7c8175a83a0748c745fd9fa8ad6da3
darkfireZZ <64635413+darkfireZZ@users.noreply.github.com>
2024-01-21 09:11:35 +0100

    Add gpg-agent.conf with pinentry config

--- nix/home.nix
+++ nix/home.nix
@@ -89,6 +89,7 @@ in {
     };
     "${config.xdg.configHome}/base16-shell".source = base16_shell;
     ".bashrc".source = "${dotfiles_dir}/home/.bashrc";
+    ".gnupg/gpg-agent.conf".source = "${dotfiles_dir}/home/.gnupg/gpg-agent.conf";
     ".mutt" = {
       source = "${dotfiles_dir}/home/.mutt";
       recursive = true;
